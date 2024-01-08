d80149e7dbf229688e2823d0c8c41429d36314aa
rassu18 <105088829+rassu18@users.noreply.github.com>
2024-01-09 00:50:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const xml2js = require('xml2js');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-// Define an object with arrays of RSS feed URLs for different categories
 const RSS_FEEDS = {
   international: ['https://www.thehindu.com/news/international/feeder/default.rss'],
   national: [
@@ -17,7 +16,6 @@ const RSS_FEEDS = {
   // Add more categories and feed URLs as needed
 };
 
-// Enable CORS for all routes
 app.use(cors());
 
 app.get('/news/:category', async (req, res) => {
@@ -28,37 +26,37 @@ app.get('/news/:category', async (req, res) => {
       return res.status(404).json({ error: 'Category not found' });
     }
 
-    // Fetch news from all RSS feeds for the specified category
     const allNews = await Promise.all(
       RSS_FEEDS[category].map(async feedUrl => {
         const response = await axios.get(feedUrl);
         const xmlData = response.data;
-
-        // Parse XML to JSON
         const jsonData = await xml2js.parseStringPromise(xmlData, { explicitArray: false });
 
-        // Extract relevant information, including images
-        return jsonData.rss.channel.item.slice(0, 9).map(item => {
-          const image =
-            item['media:content'] && item['media:content']['$'] && item['media:content']['$'].url
-              ? item['media:content']['$'].url
-              : null;
+        return jsonData.rss.channel.item
+          .slice(0, 6)
+          .filter(item => {
+            const image =
+              item['media:content'] &&
+              item['media:content']['$'] &&
+              item['media:content']['$'].url;
+            return image !== undefined && image !== null;
+          })
+          .map(item => {
+            const image =
+              item['media:content'] && item['media:content']['$'] && item['media:content']['$'].url;
 
-          return {
-            title: item.title,
-            link: item.link,
-            published: item.pubDate,
-            summary: item.description,
-            image: image,
-          };
-        });
+            return {
+              title: item.title,
+              link: item.link,
+              published: item.pubDate,
+              summary: item.description,
+              image: image,
+            };
+          });
       })
     );
 
-    // Merge news from different providers into a single array
     const newsList = allNews.flat();
-
-    // Format the data
     const formattedData = { category: category, news: newsList };
 
     res.json(formattedData);
